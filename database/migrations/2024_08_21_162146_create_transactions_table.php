01432503468e31e90d8a50e2e959f7f1b6d8a1c5
libertus-libertus <libert.jobs@gmail.com>
2024-08-22 01:10:25 +0700

    Fix: Model & Migration

--- database/migrations/2024_08_21_162146_create_transactions_table.php
+++ database/migrations/2024_08_21_162146_create_transactions_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->unsignedBigInteger('member_id')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('member_id');
+            $table->date('date_start');
+            $table->date('date_end');
             $table->timestamps();
 
             // foreignKey
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
